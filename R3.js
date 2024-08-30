6b53bd28970d542767004af4dc73763d6776dc5e
Source Academy User <No public email provided>
2024-08-30 10:13:45 +0800

    Changes made from Source Academy

--- R3.js
+++ R3.js
@@ -1,11 +1,54 @@
-const z = 1; 
+/*const z = 1; 
 function f(g) {
     const z = 3;
     return g(z);
 }
 
-f(y => y + z);
+f(y => y + z); */
 
 
 
 
+/*write a function my_sum that computes the following sum, for n=>1, without
+higher-order functions */
+ 
+ /*function my_sum(n){
+     return n < 1
+     ? 0
+     : n * (n + 1) + my_sum(n - 1);
+ }
+ 
+ my_sum(3);*/
+ 
+ 
+ function sum(term, a, next, n){
+     /* const a = 1; the const a will be fighted with the parameter a 
+     as both of them are in one scope */
+     return a  > n
+     ? 0 
+     : term(a) + sum(term, next(a), next, n);
+
+ }
+ 
+function my_sum(n){
+    const a = 1;
+     function identity(a){
+         return a * (a + 1);
+     }
+     // the function name cannot be term/ next again 
+     function plus_one(a){
+         return a + 1;
+     }
+    return sum(identity, a, plus_one, n);
+    
+}
+
+my_sum(1);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
